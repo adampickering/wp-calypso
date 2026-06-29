6e3a5b612a4dd22c9733b3e75bc77885ec0239aa
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-06-29 12:29:54 +0500

    💫 UPDATE: move count from All menu item to Automattic and P2 dropdown

--- client/reader/sidebar/reader-sidebar-recent/index.tsx
+++ client/reader/sidebar/reader-sidebar-recent/index.tsx
@@ -17,7 +17,6 @@ import { recordAction, recordGaEvent } from 'calypso/reader/stats';
 import { useRecordReaderTracksEvent } from 'calypso/state/reader/analytics/useRecordReaderTracksEvent';
 import { getSelectedRecentFeedId } from 'calypso/state/reader-ui/sidebar/selectors';
 import { AppState } from 'calypso/types';
-import { AllIcon } from '../icons/all';
 import { MenuItem, MenuItemLink } from '../menu';
 
 type Props = {
@@ -99,9 +98,6 @@ const ReaderSidebarRecent = ( {
 	};
 
 	const selectMenu = () => {
-		if ( ! isOpen ) {
-			onClick();
-		}
 		trackMenuClick( null );
 		page( '/reader' );
 	};
@@ -114,7 +110,7 @@ const ReaderSidebarRecent = ( {
 			customIcon={ <ReaderIcon className="sidebar__menu-icon" viewBox="0 0 24 11" /> }
 			disableFlyout
 			className={ clsx( 'reader-sidebar-recent', 'has-counts', className, {
-				'sidebar__menu--selected': ! isOpen && isRecentStream,
+				'sidebar__menu--selected': isRecentStream && ( ! isOpen || selectedSiteFeedId === null ),
 			} ) }
 			count={ totalUnseenCount > 0 ? totalUnseenCount : undefined }
 			icon={ null }
@@ -122,18 +118,6 @@ const ReaderSidebarRecent = ( {
 			materialIconStyle={ null }
 			expandableIconClick={ onClick }
 		>
-			<MenuItem key="all" selected={ isRecentStream && selectedSiteFeedId === null }>
-				<MenuItemLink
-					href="/reader"
-					className="sidebar__menu-link all-sites-link"
-					onClick={ () => trackMenuClick( null ) }
-				>
-					<AllIcon />
-					<span>{ translate( 'All' ) }</span>
-					{ totalUnseenCount > 0 && <Count count={ totalUnseenCount } compact /> }
-				</MenuItemLink>
-			</MenuItem>
-
 			{ sitesToShow.map( ( site ) => {
 				const displayName = getReaderSidebarSiteName( site );
 
